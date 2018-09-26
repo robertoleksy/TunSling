7433b4dcd9b64aa76596ff5f518f4e722663adb4
Robert Oleksy <robertoleksy6@gmail.com>
2018-09-26 15:57:30 +0200

    implement cSendmmsg_udp::recv()

--- src/cSendmmsg_udp.cpp
+++ src/cSendmmsg_udp.cpp
@@ -11,7 +11,6 @@ cSendmmsg_udp::~cSendmmsg_udp() {
 }
 
 size_t cSendmmsg_udp::send(const unsigned char * data, size_t data_size, const boost::asio::ip::address & adr) {
-	
     struct sockaddr_in & my_addr = msgs_q.addr[m_q_len];
     iovec & msgvec = msgs_q.msg[m_q_len];
     msghdr & msg = msgs_q.hdr[m_q_len].msg_hdr;
@@ -45,11 +44,23 @@ size_t cSendmmsg_udp::send(const unsigned char * data, size_t data_size, const b
         m_q_len = 0;
         return bytes_sended;
 	}
-	
 	return 0;
 }
 
 size_t cSendmmsg_udp::recv(unsigned char * data, size_t data_size, const boost::asio::ip::address & adr, boost::asio::ip::address & adr_out) {
-    throw std::runtime_error("not implemented");
-	return 0;
+	struct sockaddr_in remote_addr, my_addr;
+	socklen_t addrlen = sizeof(my_addr);
+
+	my_addr.sin_family = AF_INET; // host byte order
+	my_addr.sin_port = htons( m_port ); // short, network byte order
+	my_addr.sin_addr.s_addr = *((int*)(adr.to_v4().to_bytes().data()));//INADDR_ANY;
+	memset( &( my_addr.sin_zero ), '\0', 8 );
+	if (bind(m_socket,(struct sockaddr*)&my_addr, sizeof(my_addr)) == -1) // TODO
+		throw std::runtime_error("bind error");
+
+	ssize_t readed_bytes= recvfrom(m_socket, data, data_size, 0, (struct sockaddr *) &remote_addr, &addrlen);
+	adr_out = boost::asio::ip::address_v4(ntohl(remote_addr.sin_addr.s_addr));
+
+	if (readed_bytes == -1) return 0;
+	return readed_bytes;
 }
